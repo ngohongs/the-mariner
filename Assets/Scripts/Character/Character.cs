e20442eba522c88ce481d1e1ff1f7cc00077eb59
ondradolejs <ondra.dolejs@gmail.com>
2023-06-04 15:04:27 +0200

    ondralvl3done
    lvl3done

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -12,25 +12,25 @@ public class Character {
             "The Chef",
             ESkill.CHEF,
             "",
-            "The Chef is a great cook with a keen sense for spotting barrels with extra food. He'll sometimes share her finds with you!"
+            "The Chef is a great cook with a keen sense for spotting barrels with extra food. He'll sometimes share her finds with you! (Passive)"
         ),
         new Character(
             "Machio-san",
             ESkill.STREAM_SKIP,
             "",
-            "Machio-san can help your ship push through treacherous currents, but he'll need a small fee of extra food to do so."
+            "Machio-san can help your ship push through treacherous currents, but he'll need a small fee of extra food to do so. (Active)"
         ),
         new Character(
             "Hades",
             ESkill.DEATH_SKIP,
             "",
-            "Hades is a master of resurrection. He will bring you back to life, but only once."
+            "Hades is a master of resurrection. He will bring you back to life, but only once. (Passive)"
         ),
         new Character(
             "The Diplomat",
             ESkill.GET_HEALTH,
             "",
-            "Diplomat will guide you through the land without harm. It will cost you few turns though."
+            "Diplomat will guide you through the land without harm. It will cost you few turns though. (Active)"
         ),
     };
     
