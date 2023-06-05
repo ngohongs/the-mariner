cb82e8ce552d3ca90ee9b6f7f5db851820352f2f
ondradolejs <ondra.dolejs@gmail.com>
2023-06-05 20:54:21 +0200

    ONDRA
    ONDRA

---
+++ Assets/GoToLevelTwo.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Collections;
+
+
+
+public class GoToLevelTwo : MonoBehaviour
+{
+    [SerializeField] GameObject levelSelect;
+
+    [SerializeField] CanvasGroup canvasGroup;
+    
+    [SerializeField] float delayBeforeFade = 0.1f;
+
+
+    private void Start()
+    {
+        levelSelect.SetActive(false);
+    }
+
+
+    public void GoToLevelTwoScene()
+    {
+        StartCoroutine(FadeOutCanvasWithDelay());
+        SceneManager.LoadScene("Talk1"); // Replace "Level2" with the name of your level 2 scene
+    }
+    public void GoToLevelThreeScene()
+    {
+        StartCoroutine(FadeOutCanvasWithDelay());
+        SceneManager.LoadScene("Talk2");
+    }
+    public void GoToMainMenu()
+    {
+        
+        levelSelect.SetActive(false);
+    }
+    public void GoToLevelSelect()
+    {
+       levelSelect.SetActive(true);
+    }
+
+
+
+    private IEnumerator FadeOutCanvasWithDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeFade);
+
+        canvasGroup.DOFade(1f, 1f);
+        canvasGroup.blocksRaycasts = false;
+
+        yield return new WaitForSeconds(1f);
+
+        if (levelSelect.activeSelf)
+            levelSelect.SetActive(false);
+        else
+            levelSelect.SetActive(true);
+    }
+}
